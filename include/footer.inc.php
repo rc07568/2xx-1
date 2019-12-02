b67b4bc13e9cbaa3cfb216b818784a9865dd3e93
rcarver <rc07568@ravens.racc.edu>
2019-12-02 11:25:12 -0500

    added new files to header.inc.php and footer.inc.php

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -1,9 +1,72 @@
-<footer>
-  <h4>this is the footer</h4>
-  <p>we place redundent information here</p>
-  <div id="footerTagLine"> I am 7 inches left of normal</div>
-	<div class="copyright">iop project Version <?php echo VERSION; ?> | &copy; 1975 - <?php echo date('Y'); ?> Bob Carver</div>
-</footer>
-	
-</body>
-</html>
+                  
+                    <!-- landing point from jump point in header - include/header.inc.php -->
+
+                                   <!-- close the #content div | opened in include/header.inc.php -->
+                                   </div>
+
+                              <!-- close the #main div | opened in include/header.inc.php -->
+                              </div>
+
+                            <!--footer start from here-->
+                              <footer>
+                                   <div class="container">
+                                        <div class="row">
+                                             <div class="col-md-4 col-sm-6 footerleft ">
+                                                  <div class="logofooter">
+                                                 Logo
+                                                  </div>
+                                                  <p>Lorem Ipsum</p>
+                                                  <p><i class="fa fa-map-pin"></i> 10, South Second Street, Reading PA 19606, USA</p>
+                                                  <p><i class="fa fa-phone"></i> Phone (US) : +0 555 222 2323</p>
+                                                  <p><i class="fa fa-envelope"></i> E-mail : info@here.com</p>
+                                             </div>
+                                             <div class="col-md-2 col-sm-6 paddingtop-bottom">
+                                                  <h6 class="heading7">GENERAL LINKS</h6>
+                                                  <ul class="footer-ul">
+                                                       <li><a href="#"> Career</a></li>
+                                                       <li><a href="#"> Privacy Policy</a></li>
+                                                       <li><a href="#"> Terms &amp; Conditions</a></li>
+                                                       <li><a href="#"> Client Gateway</a></li>
+                                                       <li><a href="#"> Ranking</a></li>
+                                                       <li><a href="#"> Case Studies</a></li>
+                                                       <li><a href="#"> Frequently Ask Questions</a></li>
+                                                  </ul>
+                                             </div>
+                                             <div class="col-md-3 col-sm-6 paddingtop-bottom">
+                                                  <h6 class="heading7">LATEST POST</h6>
+                                                  <div class="post">
+                                                       <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
+                                                       <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
+                                                       <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
+                                                  </div>
+                                             </div>
+                                             <div class="col-md-3 col-sm-6 paddingtop-bottom">
+                                                  <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-height="300" data-small-header="false" style="margin-bottom:15px;" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                                                       <div class="fb-xfbml-parse-ignore">
+                                                            <blockquote cite="https://www.facebook.com/facebook">
+                                                                 <a style="color: #FFFFFF;" href="https://www.facebook.com/facebook">Facebook</a>
+                                                            </blockquote>
+                                                       </div>
+                                                  </div>
+                                             </div>
+                                        </div>
+                                   </div>
+                                   <div id="footerTagLine">I am 7 inches left of normal</div>
+                                   <div class="copyright">iop project - Version <?php echo VERSION; ?> | &copy; 1971 - <?php echo date('Y'); ?> Brian Savage</div>
+                              </footer>
+                              <!-- /footer -->
+
+                         <!-- close the #wrapper div | opened in include/header.inc.php -->
+                         </div>
+
+                         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+                         <script src="assets/js/jquery-1.11.3.min.js"></script>
+
+                         <!-- Include all compiled plugins (below), or include individual files as needed --> 
+                         <script src="assets/js/bootstrap.js"></script>
+
+                    <!-- close the body element | opened in include/header.inc.php -->
+                    </body>
+
+                    <!-- close the html element | opened in include/header.inc.php -->
+                    </html>
--- include/header.inc.php
+++ include/header.inc.php
@@ -1,4 +1,7 @@
 <?php
+	
+	session_start();
+	
 	// Pulls data infromation and configuration used through out the applicaton
 	require_once 'config.inc.php';
 
@@ -15,50 +18,73 @@
 
 ?>
 
-<!doctype html>
 
-	<?php
-	//this is begining of the html the browser will process
-	?>
-<html>
-	<?php
-	//information used by the brower
-	?>
-<head>
-	
-<meta charset="utf-8">
-	<?php
-	/*
-			displayed anywhere the title is used
-			for the browser search engine, etc.
-	*/
-	?>
-<title><?php echo SITENAME; ?></title>
-	<?php
-	/*
-			used to display information about the 
-			page by search engines and other sites
-			looking at out page
-	*/
-	?>
-<meta name="description" content="inside out is a class project we use to learn html." />
+      
 
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<script>
-		var siteName ='<?php echo SITENAME; ?>';
-	</script>
-	
-</head>
-	<?php
-	//	content displayed in the browser window
+               <!doctype html>
 
-	?>
+               <?php
+                    //this is begining of the html the browser will process
+               ?>
+               <html>
+                    <?php
+                         //information used by the browser
+                    ?>
+                    <head>
+                         <meta charset="utf-8">
+                         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<body>
-	<header>
-	  <h1><a href="index.php"> <?php echo SITENAME; ?></a></h1>
-	  <h2>continously falling forward in to the light...</h2>
-	  <nav>
-		<?php echo menuBuilder($content ['pages']);?>`
-	  </nav>
-	</header>
+                         <?php
+                              /*
+                                   displayed anywhere the title is used
+                                   for the browser, search engines, etc.
+                              */
+                         ?>
+                         <title><?php echo SITENAME; ?></title>
+                         <?php
+                              /*
+                                   used to display information about the
+                                   page by search engines and other sites
+                                   looking at our page.
+                              */
+                         ?>
+                         <meta name="description" content="inside out is a class project we use to learn html." />
+
+                         <!-- Bootstrap -->
+                         <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+                         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+                         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+                         <!--[if lt IE 9]>
+                                <script src="assets/js/html5shiv.min.js"></script>
+                                <script src="assets/js/respond.min.js"></script>
+                              <![endif]-->
+
+                         <!-- styles used globaly for animations in our website -->
+                         <link rel="stylesheet" href="assets/css/animate.min.css">
+
+                         <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+                         <script>
+                              var siteName = '<?php echo SITENAME; ?>';
+                         </script>
+
+                    </head>
+                    <?php
+                         //contnet displayed in the browser window
+                    ?>
+                    <body>
+                    <div id="wrapper">
+                         <header>
+                              <div class="container clearfix">
+                                   <h1 id="logo"><a href="index.php">
+                                        <a href="index.php"><?php echo SITENAME; ?></a>
+                                   </h1>
+                                   <nav id="primary_nav_wrap">
+                                        <?php echo menuBuilder($content['pages']); ?>
+                                   </nav>
+                              </div>
+                         </header>
+                         <div id="main">
+                              <div id="content">
