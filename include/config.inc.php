e037a624c60d56fa8b7aacaed17277f57dfffa0a
rcarver <rc07568@ravens.racc.edu>
2019-12-02 13:05:01 -0500

    added datatbase from localhost and remote host, updated the config.inc.php

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,11 +1,36 @@
 <?php
-// provide the current version of the application
-define("VERSION", "2.0.4.0");
 
-// provide the application name
-define("SITENAME", "inside out| 2xx");
+	// defines the default title of the website 
+	define('SITENAME','inside out | STEP 205');
 
-     
- // defines the default page of the website 
+	// defines the default page of the website 
 	define('DEFUALT_PAGE','home');
+	
+	
+	if($_SERVER['SERVER_NAME'] === 'localhost'){
+		/* Database credentials. server with default setting 
+		(user 'root' with no password) */
+		define( 'DB_SERVER', 'localhost' );
+		define( 'DB_USERNAME', 'root' );
+		define( 'DB_PASSWORD', '' );
+		define( 'DB_NAME', 'web230app_step205' );
+	}else{
+		/* Database credentials. server with default setting 
+		(user 'root' with no password) */
+		define( 'DB_SERVER', 'localhost' );
+		define( 'DB_USERNAME', 'rcarver545_205' );
+		define( 'DB_PASSWORD', 'letme!n' );
+		define( 'DB_NAME', 'rcarver545_205' );
+	}
+
+	/* Attempt to connect to MySQL database */
+	$link = mysqli_connect( DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME );
+
+	// Check connection
+	if ( $link === false ) {
+		die( "ERROR: Could not connect. " . mysqli_connect_error() );
+	
+	}
+
 ?>
+
---
+++ include/menu.data.php
@@ -0,0 +1,8 @@
+<?php
+	$menuItems = array('pages' =>
+					  array('MenuLink' => 'love.php', 'MenuName' =>'love'),
+					  array('MenuLink' => 'who.php', 'MenuName' =>'who'),
+					  array('MenuLink' => 'challenges.php', 'MenuName' =>'challenges'),
+					  array('MenuLink' => 'you.php', 'MenuName' =>'you')					  
+				  );
+?>
