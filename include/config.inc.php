845ae397aa3ffa6c3f177629a20454ec1ba45ffd
rcarver <rc07568@ravens.racc.edu>
2019-12-02 11:28:47 -0500

    udated config.inc.php

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,9 +1,11 @@
 <?php
 // provide the current version of the application
-define("VERSION", "2.0.3.0");
+define("VERSION", "2.0.4.0");
 
 // provide the application name
 define("SITENAME", "inside out| 2xx");
 
-
+     
+ // defines the default page of the website 
+	define('DEFUALT_PAGE','home');
 ?>
